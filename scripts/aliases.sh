a15663ae68de60b9d36fd8ee144e39e85a6577fe
bfordAIMS <bford@aims.gov.au>
2023-11-27 08:24:01 +0800

    test

--- scripts/aliases.sh
+++ scripts/aliases.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-alias ddev-build = 'docker build'
+# Docker development environment (dockerdev)
+alias ddev-init='if [ -d ~/.dockerdev ]; then echo "ddev already initialised at ~/.dockerdev"; else git clone https://github.com/brendevan/.dockerdev ~/.dockerdev; fi'
+
+alias ddev-update='git -C ~/.dockerdev pull'
+
+alias ddev-build='docker build ~/.dockerdev -t ddev'
